a740adef0d4aca90885f1f19746831823d60492f
Domingo1987 <domingoperez1987@gmail.com>
2023-08-02 10:01:40 -0300

    batalla naval

---
+++ Soluciones/BatallaNaval.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+# Crear un tablero de juego 5x5
+def crearTablero():
+    tablero = np.zeros((5,5))
+    return tablero
+
+# Colocar tres barcos en posiciones aleatorias
+
+    #x, y = np.random.randint(0, 5, size=2)
+
+
+# Función para verificar si hay un barco o agua en las coordenadas dadas
+
+
+# Prueba la función con algunas coordenadas
+
+
+#Programa>> a JUGAR!
+tab = crearTablero()
+print("Tablero INICIAL\n")
+print(tab)
